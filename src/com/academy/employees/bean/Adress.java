fe4c126d8d2a3951ffd29bdd608dad9e206baf60
SashaShust <akuzmenko001@gmail.com>
2019-03-17 20:33:59 +0300

    first commit

---
+++ src/com/academy/employees/bean/Adress.java
@@ -0,0 +1,87 @@
+package com.academy.employees.bean;
+
+public class Adress implements Cloneable {
+	private String country;
+	private String city;
+	private String street;
+
+	public Adress(String country, String city, String street) {
+		super();
+		this.country = country;
+		this.city = city;
+		this.street = street;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	@Override
+	public String toString() {
+		return "Adress [country=" + country + ", city=" + city + ", street=" + street + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adress other = (Adress) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+
+	@Override
+	protected Adress clone() throws CloneNotSupportedException {
+		Adress cloneObj = (Adress) super.clone();
+		return cloneObj;
+	}
+
+}
